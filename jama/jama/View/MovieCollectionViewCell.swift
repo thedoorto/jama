f7bf3d7752b802e6fa6a5306dc7f902456d971b3
Mark Brightman <mark@solutions.to>
2018-01-06 09:17:33 +0000

    adopt MVCN project structure

---
+++ jama/jama/View/MovieCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieCollectionViewCell.swift
+//  jama
+//
+//  Created by Mark Brightman on 06/01/2018.
+//  Copyright © 2018 Mark Brightman. All rights reserved.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+    
+}
